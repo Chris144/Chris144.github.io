c0b033bd96c072792dc4b29f552b1a3388b75e29
Krzysztof Pawelec <krzysztofkylu@gmail.com>
2017-12-17 12:15:22 +0100

    typy-danych

---
+++ java-script/javascript/5-datatypes.js
@@ -0,0 +1,20 @@
+'use strict';
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia;
+console.log(calkowitaWyplata);
+
+
+
+
+var czJestSmog;
+  czJestSmog = true;
+
+ if (czJestSmog){
+     console.log("Jest SMOG");
+ }
+else {
+    cosnole.log("nie ma SMOGA");
+}
