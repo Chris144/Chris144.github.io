6a5410b252c585af6689f3468f220bda32c5a1ef
Krzysztof Pawelec <krzysztofkylu@gmail.com>
2017-12-17 15:27:45 +0100

    warunki

---
+++ java-script/javascript/10-conditions.js
@@ -0,0 +1,37 @@
+'use strict';
+var wzrostOli = 191;
+var wzrostDamiana = 191;
+
+ if (wzrostOli > wzrostDamiana)
+   {
+    
+       console.log("Olga jest wyzsza:)");
+       
+   }
+   else if (wzrostOli == wzrostDamiana)
+   {
+       console.log ("Olga jest rowna jak damian :")
+   }
+   else {
+       console.log ("olga jest rnizsza")
+   }
+
+
+
+
+
+var kolor="zielony";
+
+switch (kolor){
+    case 'czerwony':
+        console.log('kolor czerwony')
+        break;
+    case 'zielony':
+        console.log('kolor zielony')
+        break;
+    case 'niebieski':
+        console.log('kolor niebieski')
+        break;
+    default:
+    console.log('Inny kolor')
+}
