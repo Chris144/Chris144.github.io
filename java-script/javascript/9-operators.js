4a99b460a242bb167a2f5e879a98b07c4ada05bb
Krzysztof Pawelec <krzysztofkylu@gmail.com>
2017-12-17 14:55:31 +0100

    operatory

---
+++ java-script/javascript/9-operators.js
@@ -0,0 +1,21 @@
+'use strict';
+var liczba1 =13, liczba2=5;
+
+var wynik;
+
+wynik=liczba1 % liczba2;
+console.log(wynik);
+
+liczba1-=liczba2;
+
+console.log(liczba1);
+
+
+if("2" === 2)
+    {
+        console.log("prawda");
+        
+    }
+    else{
+        console.log("nie prawda");
+        document.write("<br> prawda");    }
