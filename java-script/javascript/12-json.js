67dbf0e7c76ea3fcdc55ec7003394c6ed570722e
Krzysztof Pawelec <krzysztofkylu@gmail.com>
2017-12-17 16:32:57 +0100

    json

---
+++ java-script/javascript/12-json.js
@@ -0,0 +1,47 @@
+  'use strict';
+var jsonOsoby = {
+  
+     "osoby": [
+         {
+             imie: "Krystian",
+             nazwisko: "Dziopa",
+             wzrost: 180,
+             oczy: "niebieskie",
+             zainteresowania: [
+                 { nazwa: "podroze" },
+                 { nazwa: "gotowanie" }
+             ]
+         },
+         {
+             imie: "Dominik",
+             nazwisko: "Gomuła",
+             wzrost: 170,
+             oczy: "brązowe",
+             zainteresowania: [
+                 { nazwa: "gotowanie" },
+                 { nazwa: "języki" }
+             ]
+         },
+         {
+             imie: "Claudia",
+             nazwisko: "Żelazowska",
+             wzrost: 167,
+             oczy: "brązowe",
+             zainteresowania: [
+                 { nazwa: "programowanie" },
+                 { nazwa: "podroze" },
+                 { nazwa: "języki" }
+             ]
+
+         }
+     ]
+}
+
+
+jsonOsoby.osoby.forEach(function(e){
+    console.log(e.imie);
+    
+jsonOsoby.osoby.forEach(function(e){
+    console.log(e.wzrost);
+})
+});
