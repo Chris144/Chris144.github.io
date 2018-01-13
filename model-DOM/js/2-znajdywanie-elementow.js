b6badcc03b3a087039adf23d5c38cd82b2bb4288
Krzysztof Pawelec <krzysztofkylu@gmail.com>
2018-01-13 10:24:17 +0100

    znajdywanie-elementow

---
+++ model-DOM/js/2-znajdywanie-elementow.js
@@ -0,0 +1,11 @@
+'use strict';
+//var parFirst= document.getElementById('parFirst');
+//console.log(parFirst);
+//var linki = document.getElementsByClassName("superlink");
+//console.log(linki);
+//var linkiWszystkie = document.getElementsByTagName("a");
+//console.log(linkiWszystkie);
+//var divPoId = document.querySelector("#parSecond");
+//console.log(divPoId);
+var superLinki = document.querySelectorAll(".link");
+console.log(superLinki);
