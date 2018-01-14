294e7a4f31649a15cdaaa9a80166e6f0beed37d7
Krzysztof Pawelec <krzysztofkylu@gmail.com>
2018-01-14 11:17:18 +0100

    selektory

---
+++ jQuery/js/2-selektory.js
@@ -0,0 +1,14 @@
+$(function(){
+    'use strict';
+    
+    var paragrafy =$("p");
+    console.log(paragrafy);
+    
+    var h2 = $("#second");
+    console.log(h2);
+    
+    var paragraf =$(".paragraf");
+        console.log(paragraf);
+    
+    
+});
