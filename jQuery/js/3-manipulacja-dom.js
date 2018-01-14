4a55ba23630e205429a78e21d77ec7e6e41e0d07
Krzysztof Pawelec <krzysztofkylu@gmail.com>
2018-01-14 12:38:42 +0100

    manipilacja-dom

---
+++ jQuery/js/3-manipulacja-dom.js
@@ -0,0 +1,34 @@
+ /* 'use strict';
+$(function(){
+    'use strict';
+  
+   
+   // var textParagrafu = $("p#paragraf").text("Nowy paragraf o id paragraf");
+   // console.log( $("p#paragraf").text());
+
+
+var jakisTamParafgraf = $("p.paragrafnext");
+jakisTamParafgraf.html("<b>Hello</b>World").after("Treść za elementem");
+    
+$(".paragrafnext").append("<br> Dodatkowy tekst paragrafu");
+    
+$( "input#imie" ).val( "Krzysztof" ); 
+
+    
+    $("h3").remove();
+    $("h2").empty();
+    
+$("h2").html("<span style=color:red;'>Kolorowy span w h2</span>");
+    
+$("h1").css("font-size","100px");
+    
+var imie= $("input#imie").val(); 
+    console.log(imie);
+
+    $("h1").attr //pobiera atrybut
+$("h1").attr("title","Nagłowek h1");
+});
+*/
+$("h1").addClass("blue");
+
+$(".paragrafnext").removeClass("paragrafnext");
