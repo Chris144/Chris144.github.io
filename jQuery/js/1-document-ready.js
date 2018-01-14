5582b4dab9b3287b6caf5224bc248c5193dccd5b
Krzysztof Pawelec <krzysztofkylu@gmail.com>
2018-01-14 10:55:35 +0100

    document-ready

---
+++ jQuery/js/1-document-ready.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//zapewnie ze nasz model Dom sie wczyta
+//$(document).ready(function(){
+  //  alert("Czołem wczytałem DOM i jestem jQuery");
+//});
+
+//$("#paragraf").css("color","red");
+$ function(){
+$("paragraf").css
+
+(
+    {"color": "red",
+     "bacground-color":"#000"
+                  });
+    
+});
